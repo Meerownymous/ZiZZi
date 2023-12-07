a7818542dc9207b6b01086cb57b6ff64cb364658
meerow <meerow@mirmx.de>
2023-12-07 23:27:19 +0100

    feat: object block list

--- src/ZiZZi/Matter/Object/ObjectBlockList.cs
+++ src/ZiZZi/Matter/Object/ObjectBlockList.cs
@@ -1,37 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Tonga.Enumerable;
 
 namespace ZiZZi.Matter.Object
 {
-    public sealed class ObjectBlockList<TBlock> : IMatter<TBlock>
+    /// <summary>
+    /// A list of blocks in shape of the given generic parameter.
+    /// </summary>
+    public sealed class ObjectBlockList<TBlock> : IMatter<object>
     {
-        public ObjectBlockList()
+        private readonly TBlock blueprint;
+        private readonly BytesAsTyped conversion;
+        private readonly IList<dynamic> items;
+
+        /// <summary>
+        /// A list of blocks in shape of the given generic parameter.
+        /// </summary>
+        public ObjectBlockList(TBlock blueprint, BytesAsTyped conversion)
         {
+            this.blueprint = blueprint;
+            this.conversion = conversion;
+            this.items = new List<dynamic>();
         }
 
-        public TBlock Content()
+        public dynamic Content()
         {
-            throw new NotImplementedException();
+            return
+                Mapped._(
+                    item => (item as IMatter<dynamic>).Content(),
+                    this.items
+                ).ToArray();
         }
 
-        public IMatter<TBlock> Open(string contentType, string name)
+        public IMatter<dynamic> Open(string contentType, string name)
         {
-            throw new NotImplementedException();
+            dynamic sub =
+                Activator.CreateInstance(
+                    typeof(ObjectBlock2<>)
+                        .MakeGenericType(typeof(TBlock)),
+                        this.blueprint,
+                        this.conversion
+                );
+            this.items.Add(sub);
+            return sub;
         }
 
         public void Present(string name, Func<IContent<string>> content)
         {
-            throw new NotImplementedException();
+            throw new InvalidOperationException();
         }
 
         public void Present(string name, string dataType, Func<IContent<byte[]>> content)
         {
-            throw new NotImplementedException();
+            throw new InvalidOperationException();
         }
 
         public void Present(string name, string dataType, Func<IContent<Stream>> content)
         {
-            throw new NotImplementedException();
+            throw new InvalidOperationException();
         }
     }
 }
---
+++ tests/Test.ZiZZi/Matter/Object/ObjectBlockListTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Dynamic;
+using Tonga.Bytes;
+using Xunit;
+
+namespace ZiZZi.Matter.Object.Test
+{
+    public sealed class ObjectBlockListTests
+    {
+        [Fact]
+        public void AddsBlocksToList()
+        {
+            dynamic parent = new ExpandoObject();
+            var users = new string[] { "Bob", "Jay" };
+            var blueprint = new { ID = 0, Name = "" };
+
+            Type listType =
+                typeof(ObjectBlockList<>)
+                    .MakeGenericType(blueprint.GetType());
+
+            dynamic block =
+                Activator.CreateInstance(
+                    listType,
+                    blueprint,
+                    new BytesAsTyped()
+                );
+
+            for(var i=0;i<users.Length;i++)
+            {
+                var user = block.Open("block-inside-list", "User");
+                Func<IContent<string>> name = () => TakeContent._(users[i]);
+                Func<IContent<byte[]>> id = () => TakeContent._(BitConverter.GetBytes(i));
+                user.Present("Name", name);
+                user.Present("ID", "int32", id);
+            }
+
+            for (var i = 0; i < users.Length; i++)
+            {
+                Assert.Equal(users[i], block.Content()[i].Name);
+            }
+        }
+    }
+}
